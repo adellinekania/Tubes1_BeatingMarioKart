c9cd27023d5c71be2ce11797d3bf73629c7d741e
adellinekania <adellinekaniasetiyawan@gmail.com>
2022-02-11 23:04:54 +0700

    Power Ups Adellll

--- src/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ src/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -2,42 +2,163 @@ package za.co.entelect.challenge;
 
 import za.co.entelect.challenge.command.*;
 import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.PowerUps;
 import za.co.entelect.challenge.enums.Terrain;
 
 import java.util.*;
 
 import static java.lang.Math.max;
 
+import java.security.SecureRandom;
+
 public class Bot {
 
     private static final int maxSpeed = 9;
-    private List<Integer> directionList = new ArrayList<>();
+    private List<Command> directionList = new ArrayList<>();
+    private int countTweet = 0;
 
-    private Random random;
-    private GameState gameState;
-    private Car opponent;
-    private Car myCar;
+    private final Random random;
+
+    private final static Command ACCELERATE = new AccelerateCommand();
+    private final static Command LIZARD = new LizardCommand();
+    private final static Command OIL = new OilCommand();
+    private final static Command BOOST = new BoostCommand();
+    private final static Command EMP = new EmpCommand();
     private final static Command FIX = new FixCommand();
 
-    public Bot(Random random, GameState gameState) {
-        this.random = random;
-        this.gameState = gameState;
-        this.myCar = gameState.player;
-        this.opponent = gameState.opponent;
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
 
-        directionList.add(-1);
-        directionList.add(1);
+    public Bot() {
+        this.random = new SecureRandom();
+        directionList.add(TURN_LEFT);
+        directionList.add(TURN_RIGHT);
     }
 
-    public Command run() {
-        return FIX;
+    public Command run(GameState gameState) {
+        Car myCar = gameState.player;
+        Car opponent = gameState.opponent;
+
+        // Basic fix logic
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        List<Object> nextBlocks = blocks.subList(0, 1);
+
+        // Fix first if too damaged to move
+        if (myCar.damage == 5) {
+            return FIX;
+        }
+        // Accelerate first if going to slow
+        if (myCar.speed <= 3) {
+            return ACCELERATE;
+        }
+
+        // Basic fix logic
+        if (myCar.damage >= 5) {
+            return FIX;
+        }
+
+        // Basic avoidance logic
+        if (blocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+                return LIZARD;
+            }
+            if (nextBlocks.contains(Terrain.MUD) || nextBlocks.contains(Terrain.WALL)) {
+                int i = random.nextInt(directionList.size());
+                return directionList.get(i);
+            }
+        }
+
+        // PUNYA ADELLL : POWERUPSS
+        // EMP
+        if (hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+            if (opponent.speed > 5) {
+                if (opponent.position.block > myCar.position.block) {
+                    int yOp = opponent.position.lane;
+                    int yMy = myCar.position.lane;
+                    if (yMy == yOp || yMy == yOp + 1 || yMy == yOp - 1) {
+                        return EMP;
+                    }
+                }
+            }
+        }
+
+        int countTweet = 0;
+        if (hasPowerUp(PowerUps.TWEET, myCar.powerups)) {
+            if (opponent.speed > 5) {
+                if (countTweet == 0) {
+                    countTweet++;
+                    return new TweetCommand(4, 76);
+                } else {
+                    int yOp = opponent.position.lane;
+                    int xOp = opponent.position.block;
+                    int speedOp = opponent.speed;
+                    return new TweetCommand(yOp, xOp + speedOp + 1);
+                }
+            }
+        }
+
+        // BOOST
+        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+            if (myCar.damage == 0) {
+                if (!(blocks.contains(Terrain.MUD) || blocks.contains(Terrain.WALL)
+                        || blocks.contains(Terrain.OIL_SPILL))) {
+                    return BOOST;
+                } else {
+                    if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+                        return BOOST;
+                    } else {
+                        if (myCar.position.lane == 2 || myCar.position.lane == 3) {
+                            List<Object> leftBlocks = getBlocksInFront(myCar.position.lane - 1, myCar.position.block,
+                                    gameState);
+                            List<Object> rightBlocks = getBlocksInFront(myCar.position.lane + 1, myCar.position.block,
+                                    gameState);
+                            if ((!(leftBlocks.contains(Terrain.MUD) || leftBlocks.contains(Terrain.WALL)
+                                    || leftBlocks.contains(Terrain.OIL_SPILL)))
+                                    || (!(rightBlocks.contains(Terrain.MUD) || rightBlocks.contains(Terrain.WALL)
+                                            || rightBlocks.contains(Terrain.OIL_SPILL)))) {
+                                return BOOST;
+                            }
+                        } else if (myCar.position.lane == 1) {
+                            List<Object> secondBlocks = getBlocksInFront(2, myCar.position.block, gameState);
+                            if (!(secondBlocks.contains(Terrain.MUD) || secondBlocks.contains(Terrain.WALL)
+                                    || secondBlocks.contains(Terrain.OIL_SPILL))) {
+                                return BOOST;
+                            }
+                        } else if (myCar.position.lane == 4) {
+                            List<Object> thirdBlocks = getBlocksInFront(3, myCar.position.block, gameState);
+                            if (!(thirdBlocks.contains(Terrain.MUD) || thirdBlocks.contains(Terrain.WALL)
+                                    || thirdBlocks.contains(Terrain.OIL_SPILL))) {
+                                return BOOST;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (hasPowerUp(PowerUps.OIL, myCar.powerups) && (myCar.position.block > opponent.position.block)) {
+            return OIL;
+        }
+
+        System.out.println("HADEHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+
+        return ACCELERATE;
+    }
+
+    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
+        for (PowerUps powerUp : available) {
+            if (powerUp.equals(powerUpToCheck)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
-     * Returns map of blocks and the objects in the for the current lanes, returns the amount of blocks that can be
-     * traversed at max speed.
+     * Returns map of blocks and the objects in the for the current lanes, returns
+     * the amount of blocks that can be traversed at max speed.
      **/
-    private List<Object> getBlocksInFront(int lane, int block) {
+    private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
         List<Lane[]> map = gameState.lanes;
         List<Object> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
